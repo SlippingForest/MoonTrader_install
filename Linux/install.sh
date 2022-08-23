af7931125de19b3bef53d64167d59a2620a0e98b
SlippingForest <autcores@gmail.com>
2022-08-23 03:30:46 +0300

    add config file for chrony

--- Linux/install.sh
+++ Linux/install.sh
@@ -9,29 +9,29 @@ function color_echo {
     text_cyan="\e[0;36m"
     text_magenta="\e[95m"
     text_dafault="\e[0m"
-    
+
     case $1 in
-        "red")
-            echo -e "${text_red}$2${text_dafault}"
+    "red")
+        echo -e "${text_red}$2${text_dafault}"
         ;;
-        "gold")
-            echo -e "${text_gold}$2${text_dafault}"
+    "gold")
+        echo -e "${text_gold}$2${text_dafault}"
         ;;
-        "green")
-            echo -e "${text_green}$2${text_dafault}"
+    "green")
+        echo -e "${text_green}$2${text_dafault}"
         ;;
-        "cyan")
-            echo -e "${text_cyan}$2${text_dafault}"
+    "cyan")
+        echo -e "${text_cyan}$2${text_dafault}"
         ;;
-        "magenta")
-            echo -e "${text_magenta}$2${text_dafault}"
+    "magenta")
+        echo -e "${text_magenta}$2${text_dafault}"
         ;;
-        "title")
-            text_cyan=$(tput setaf 6)
-            text_dafault=$(tput sgr0)
-            COLUMNS=$(tput cols)
-            title=$2
-            printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "${text_cyan}$title${text_dafault}"
+    "title")
+        text_cyan=$(tput setaf 6)
+        text_dafault=$(tput sgr0)
+        COLUMNS=$(tput cols)
+        title=$2
+        printf "%*s\n" $(((${#title} + $COLUMNS) / 2)) "${text_cyan}$title${text_dafault}"
         ;;
     esac
 }
@@ -39,7 +39,7 @@ function color_echo {
 # Проверка операционной системы
 # Return
 # OS_NAME - operating system name
-function check_os(){
+function check_os() {
     if [ -f /etc/os-release ]; then
         . /etc/os-release
         if [ "$ID" = "debian" ]; then
@@ -120,12 +120,11 @@ function select_user() {
 #     color_echo green "Default user directory: $default_user_directory"
 # }
 
-
 # Выбор способа установки
 # Return
 # mt_link - installation archive link
 # mt_extension - installation archive extension
-function select_installation(){
+function select_installation() {
     color_echo title "Select installation type"
     install_type_list=(
         "Automatic installation of the latest version"
@@ -135,46 +134,45 @@ function select_installation(){
     )
     color_echo magenta "Select installation option"
     PS3="Enter number option: "
-    select install_type in "${install_type_list[@]}"
-    do
+    select install_type in "${install_type_list[@]}"; do
         case $install_type in
-            "Automatic installation of the latest version")
-                mt_link="https://cdn3.moontrader.com/beta/linux-x86_64/MoonTrader-linux-x86_64.tar.xz"
-                mt_extention=".tar.xz"
-                break
+        "Automatic installation of the latest version")
+            mt_link="https://cdn3.moontrader.com/beta/linux-x86_64/MoonTrader-linux-x86_64.tar.xz"
+            mt_extention=".tar.xz"
+            break
             ;;
-            "Install using the Dropbox link [.tar.xz]")
+        "Install using the Dropbox link [.tar.xz]")
+            color_echo gold "[EXAMPLE] https://www.dropbox.com/s/.../archive_name.tar.xz?dl=0"
+            read -p "paste DropBox link [.tar.xz]: " mt_link
+            while ! [[ $mt_link == *".tar.xz"* ]]; do
+                clear
+                color_echo red "[ERROR] Wrong link"
                 color_echo gold "[EXAMPLE] https://www.dropbox.com/s/.../archive_name.tar.xz?dl=0"
                 read -p "paste DropBox link [.tar.xz]: " mt_link
-                while ! [[ $mt_link == *".tar.xz"* ]]; do
-                    clear
-                    color_echo red "[ERROR] Wrong link"
-                    color_echo gold "[EXAMPLE] https://www.dropbox.com/s/.../archive_name.tar.xz?dl=0"
-                    read -p "paste DropBox link [.tar.xz]: " mt_link
-                done
-                mt_link=${mt_link%?}1
-                mt_extention=".tar.xz"
-                break
+            done
+            mt_link=${mt_link%?}1
+            mt_extention=".tar.xz"
+            break
             ;;
-            "Install using the Dropbox link [.7z]")
+        "Install using the Dropbox link [.7z]")
+            color_echo gold "[EXAMPLE] https://www.dropbox.com/s/.../archive_name.7z?dl=0"
+            read -p "paste DropBox link [.7z]: " mt_link
+            while ! [[ $mt_link == *".7z"* ]]; do
+                clear
+                color_echo red "[ERROR] Wrong link"
                 color_echo gold "[EXAMPLE] https://www.dropbox.com/s/.../archive_name.7z?dl=0"
                 read -p "paste DropBox link [.7z]: " mt_link
-                while ! [[ $mt_link == *".7z"* ]]; do
-                    clear
-                    color_echo red "[ERROR] Wrong link"
-                    color_echo gold "[EXAMPLE] https://www.dropbox.com/s/.../archive_name.7z?dl=0"
-                    read -p "paste DropBox link [.7z]: " mt_link
-                done
-                mt_link=${mt_link%?}1
-                mt_extention=".7z"
-                break
+            done
+            mt_link=${mt_link%?}1
+            mt_extention=".7z"
+            break
             ;;
-            "Exit")
-                color_echo red "Exit script"
-                exit 1
+        "Exit")
+            color_echo red "Exit script"
+            exit 1
             ;;
-            *)
-                color_echo red "[ERROR] Wrong answer, try again"
+        *)
+            color_echo red "[ERROR] Wrong answer, try again"
             ;;
         esac
     done
@@ -182,10 +180,10 @@ function select_installation(){
     color_echo green "Install link: $mt_link"
 }
 
-function install_packages(){
+function install_packages() {
     # Установка пакетов
     color_echo title "Installing requaired packages"
-    apt update > /dev/null 2>&1
+    apt update >/dev/null 2>&1
     add_pkgs=(
         fail2ban
         chrony
@@ -201,39 +199,54 @@ function install_packages(){
         dotnet-sdk-6.0
     )
     for pkg in "${add_pkgs[@]}"; do
-        if ! dpkg -s $pkg > /dev/null 2>&1; then
+        if ! dpkg -s $pkg >/dev/null 2>&1; then
             color_echo green "Install $pkg"
             case $pkg in
-                "fail2ban")
-                    apt install -y $pkg
-                    touch /etc/fail2ban/jail.local
-                    service fail2ban restart
-                    echo -e "[sshd]\nport = ssh\nfindtime = 3600\nmaxretry = 3\nbantime = 86400" > /etc/fail2ban/jail.local
+            "chrony")
+                apt install -y $pkg
+                echo -e "server 0.jp.pool.ntp.org iburst" > /etc/chrony/chrony.conf
+                echo -e "server 1.jp.pool.ntp.org iburst" >> /etc/chrony/chrony.conf
+                echo -e "server 2.jp.pool.ntp.org iburst" >> /etc/chrony/chrony.conf
+                echo -e "server 3.jp.pool.ntp.org iburst" >> /etc/chrony/chrony.conf
+                echo -e "minsources 3" >> /etc/chrony/chrony.conf
+                echo -e "maxchange 100 0 0" >> /etc/chrony/chrony.conf
+                echo -e "makestep 0.001 1" >> /etc/chrony/chrony.conf
+                echo -e "maxdrift 100" >> /etc/chrony/chrony.conf
+                echo -e "maxslewrate 100" >> /etc/chrony/chrony.conf
+                echo -e "driftfile /var/lib/chrony/drift" >> /etc/chrony/chrony.conf
+                echo -e "rtcsync" >> /etc/chrony/chrony.conf
+                service chrony restart
                 ;;
-                "libtommath1")
-                    apt install -y $pkg
-                    if [ -f "/usr/lib/x86_64-linux-gnu/libtommath.so.0" ]; then
-                        rm /usr/lib/x86_64-linux-gnu/libtommath.so.0
-                    fi
-                    ln -s libtommath.so.1 /usr/lib/x86_64-linux-gnu/libtommath.so.0
+            "fail2ban")
+                apt install -y $pkg
+                touch /etc/fail2ban/jail.local
+                echo -e "[sshd]\nport = ssh\nfindtime = 3600\nmaxretry = 3\nbantime = 86400" >/etc/fail2ban/jail.local
+                service fail2ban restart
                 ;;
-                "dotnet-sdk-6.0")
-                    if [ "$OS_NAME" == "Debian 10" ]; then
-                        wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-                    elif [ "$OS_NAME" == "Debian 11" ]; then
-                        wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-                    elif [ "$OS_NAME" == "Ubuntu 20.04" ]; then
-                        wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-                    elif [ "$OS_NAME" == "Ubuntu 22.04" ]; then
-                        wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-                    fi
-                    dpkg -i packages-microsoft-prod.deb
-                    rm packages-microsoft-prod.deb
-                    apt update
-                    apt install -y dotnet-sdk-6.0
+            "libtommath1")
+                apt install -y $pkg
+                if [ -f "/usr/lib/x86_64-linux-gnu/libtommath.so.0" ]; then
+                    rm /usr/lib/x86_64-linux-gnu/libtommath.so.0
+                fi
+                ln -s libtommath.so.1 /usr/lib/x86_64-linux-gnu/libtommath.so.0
                 ;;
-                *)
-                    DEBIAN_FRONTEND=noninteractive apt install -y $pkg
+            "dotnet-sdk-6.0")
+                if [ "$OS_NAME" == "Debian 10" ]; then
+                    wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+                elif [ "$OS_NAME" == "Debian 11" ]; then
+                    wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+                elif [ "$OS_NAME" == "Ubuntu 20.04" ]; then
+                    wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+                elif [ "$OS_NAME" == "Ubuntu 22.04" ]; then
+                    wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+                fi
+                dpkg -i packages-microsoft-prod.deb
+                rm packages-microsoft-prod.deb
+                apt update
+                apt install -y dotnet-sdk-6.0
+                ;;
+            *)
+                DEBIAN_FRONTEND=noninteractive apt install -y $pkg
                 ;;
             esac
             color_echo green "complete \n"
@@ -243,12 +256,12 @@ function install_packages(){
     done
 }
 
-function remove_packages(){
+function remove_packages() {
     # Удаление ненужных пакетов
     color_echo title "Removing unused packages"
     rm_pkgs=(snapd exim exim4)
     for pkg in "${rm_pkgs[@]}"; do
-        if dpkg -s $pkg > /dev/null 2>&1; then
+        if dpkg -s $pkg >/dev/null 2>&1; then
             color_echo green "Remove $pkg"
             if [ $pkg == "snapd" ]; then
                 apt -yqq remove $pkg --purge
@@ -265,15 +278,15 @@ function remove_packages(){
     done
 }
 
-function update_packages(){
+function update_packages() {
     # Обновление пакетов
     color_echo title "Updating packages"
-    apt update > /dev/null 2>&1
+    apt update >/dev/null 2>&1
     DEBIAN_FRONTEND=noninteractive apt upgrade -y
     color_echo green "Updating packages complete"
 }
 
-function create_mt_folder(){
+function create_mt_folder() {
     # Создание папки с MoonTrader
     color_echo title "Createing MoonTrader directory"
     mt_folder="MoonTrader"
@@ -286,7 +299,7 @@ function create_mt_folder(){
     color_echo green "MoonTrader directory: $default_user_directory/$mt_folder"
 }
 
-function enable_swap(){
+function enable_swap() {
     # Включение swap если он выключен и RAM меньше 2Gb
     color_echo title "Enabling swap"
     if free | awk '/^Swap:/ {exit !$2}'; then
@@ -306,9 +319,9 @@ function enable_swap(){
 }
 
 # Включение и настройка firewall
-function setup_firewall(){
+function setup_firewall() {
     color_echo title "Enabling firewall"
-    if ! iptables -nL | grep 4242 > /dev/null 2>&1; then
+    if ! iptables -nL | grep 4242 >/dev/null 2>&1; then
         iptables -F
         iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
         iptables -A INPUT -m state --state INVALID -j DROP
@@ -322,10 +335,10 @@ function setup_firewall(){
     else
         color_echo green "Security already enabled"
     fi
-    
+
 }
 
-function install_mt(){
+function install_mt() {
     # Установка MoonTrader
     color_echo title "Installing MoonTrader"
     if [ $mt_extention == ".tar.xz" ]; then
@@ -335,26 +348,26 @@ function install_mt(){
         wget -O MoonTrader-linux-x86_64.7z $mt_link && 7z x "-o${default_user_directory}/${mt_folder}" MoonTrader-linux-x86_64.7z
         rm MoonTrader-linux-x86_64.7z
     fi
-    
+
     # Создание файла запуска MTCore для корректного обновления MoonTrader при запуске из usr/bin
     if [ -f "$default_user_directory/$mt_folder/MTCore" ] && [ ! -f "$default_user_directory/$mt_folder/start_mt.sh" ]; then
         chmod +x "$default_user_directory/$mt_folder/MTCore"
         touch $default_user_directory/$mt_folder/start_mt.sh && chmod +x $default_user_directory/$mt_folder/start_mt.sh
         # ToDo сделать запись строк в файл как-то красивее...
-        echo -e "if [ -n \"\$1\" ]; then" > $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "    if [ \"\$1\" == \"--no-update\" ]; then" >> $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "        cd $default_user_directory/$mt_folder" >> $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "        ./MTCore --no-update" >> $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "    else" >> $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "        cd $default_user_directory/$mt_folder" >> $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "        $default_user_directory/$mt_folder/MTCore" >> $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "    fi" >> $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "else" >> $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "    cd $default_user_directory/$mt_folder" >> $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "    $default_user_directory/$mt_folder/MTCore" >> $default_user_directory/$mt_folder/start_mt.sh
-        echo -e "fi" >> $default_user_directory/$mt_folder/start_mt.sh
-        
-        rm /usr/bin/MoonTrader > /dev/null 2>&1
+        echo -e "if [ -n \"\$1\" ]; then" >$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "    if [ \"\$1\" == \"--no-update\" ]; then" >>$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "        cd $default_user_directory/$mt_folder" >>$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "        ./MTCore --no-update" >>$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "    else" >>$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "        cd $default_user_directory/$mt_folder" >>$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "        $default_user_directory/$mt_folder/MTCore" >>$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "    fi" >>$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "else" >>$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "    cd $default_user_directory/$mt_folder" >>$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "    $default_user_directory/$mt_folder/MTCore" >>$default_user_directory/$mt_folder/start_mt.sh
+        echo -e "fi" >>$default_user_directory/$mt_folder/start_mt.sh
+
+        rm /usr/bin/MoonTrader >/dev/null 2>&1
         ln -s "$default_user_directory/$mt_folder/start_mt.sh" /usr/bin/MoonTrader
     fi
     chown -R $default_user:$default_user "$default_user_directory/$mt_folder"
